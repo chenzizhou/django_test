e32530970fe016db78fc21d7f1c5f31e1dc0f646
陈自州 <95033787+chenzizhou@users.noreply.github.com>
2025-09-17 17:38:29 +0800

    Update views.py

--- app/views.py
+++ app/views.py
@@ -1,7 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
-from app.models import User
+from app.models import User, Account
 
 # Create your views here.
 app_name = 'app'
@@ -19,3 +19,10 @@ def register_view(request):
     u = User(username='nature', password='nature')
     u.save()
     return HttpResponse("注册成功")
+
+
+def add_account_view(request):
+    u = User.objects.get(pk=1)
+    a = Account(user=u, account='c60068129', password='Czr1234567890.')
+    a.save()
+    return  HttpResponse("添加账号成功")
