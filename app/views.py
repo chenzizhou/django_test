ea5b63999796320450d4f6e6db7a9f1664b3a1f1
陈自州 <95033787+chenzizhou@users.noreply.github.com>
2025-09-22 20:44:23 +0800

    Update views.py

--- app/views.py
+++ app/views.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.http import HttpResponse
 from django.shortcuts import render
 
@@ -12,7 +13,9 @@ def index_view(request):
 
 
 def login_view(request):
-    return HttpResponse("登录界面")
+    str = '%s<br>%s' % (request.path, request.encoding)
+    print(str)
+    return render(request, 'app/login.html')
 
 
 def register_view(request):
@@ -21,8 +24,21 @@ def register_view(request):
     return HttpResponse("注册成功")
 
 
+def pic_upload(request):
+    return render(request, 'app/pic_upload.html')
+
+
 def add_account_view(request):
     u = User.objects.get(pk=1)
     a = Account(user=u, account='c60068129', password='Czr1234567890.')
     a.save()
-    return  HttpResponse("添加账号成功")
+    return HttpResponse("添加账号成功")
+
+def pic_handle(request):
+    f1=request.FILES.get('pic')
+    print(f1.name)
+    fname='%s\\app\\%s'%(settings.MEDIA_ROOT,f1.name)
+    with open(fname,'wb') as pic:
+        for c in f1.chunks(): #将文件内容写入服务器
+            pic.write(c)
+    return HttpResponse('OK')
