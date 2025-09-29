78b964510306cbd5e30f0a46138a0118ebf6da31
陈自州 <95033787+chenzizhou@users.noreply.github.com>
2025-09-29 15:59:20 +0800

    Update urls.py

--- booktest/urls.py
+++ booktest/urls.py
@@ -4,7 +4,14 @@ from booktest import views
 
 app_name = 'booktest'  # 命名空间
 urlpatterns = [
-    path('index/', views.index, name='index'),
+    path('', views.index, name='index'),
     path('cookie_set/', views.cookie_set, name='cookie_set'),
     path('session_test/', views.session_test, name='session_test'),
+    path('create/', views.create, name='create'),
+    path('delete<id>/', views.delete, name='delete'),
+    path('list<id>/', views.list, name='list'),
+    path('json1/', views.json1, name='json1'),
+    path('json2/', views.json2, name='json2'),
+    path('guolvqi/', views.guolvqi, name='guolvqi'),
+
 ]
