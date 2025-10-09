8aef0c503e354dae4bda92d4a94ccde761d53f6d
陈自州 <95033787+chenzizhou@users.noreply.github.com>
2025-10-09 20:33:46 +0800

    Update models.py

--- user/models.py
+++ user/models.py
@@ -1,12 +1,13 @@
-from django.db import models
+import hashlib
+import time
+from random import random
 
-# Create your models here.
 from django.db import models
 
 
 # Create your models here.
 class User(models.Model):
-    user_id = models.AutoField(verbose_name='用户id', primary_key=True,
+    user_id = models.BigAutoField(verbose_name='用户id', primary_key=True,
                                help_text='用户id',
                                db_comment='用户id')  # verbase_name在Django管理后台显示的字段名称，help_text在Django管理后台显示的字段提示信息
     username = models.CharField(max_length=20, unique=True, help_text='用户名')
@@ -28,3 +29,18 @@ class UserExtension(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='extension')
     phone = models.CharField(max_length=20)
     address = models.CharField(max_length=100)
+
+
+class RememberToken(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='remember_tokens')
+    token = models.CharField(max_length=64)
+    expires = models.IntegerField()  # 存储Unix时间戳
+
+    def save(self, *args, **kwargs):
+        if not self.token:
+            # 生成随机token
+            self.token = hashlib.sha256(str(random.getrandbits(256)).encode()).hexdigest()
+        if not self.expires:
+            # 设置默认过期时间为14天后
+            self.expires = time.time() + 14 * 24 * 60 * 60
+        super().save(*args, **kwargs)
