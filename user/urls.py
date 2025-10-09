80d6f6eb7a5b00820d7543b78cf05c48ef9d5922
陈自州 <95033787+chenzizhou@users.noreply.github.com>
2025-10-09 20:31:51 +0800

    Update urls.py

--- user/urls.py
+++ user/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path
 
-from user.views import login_view
+from user.views import login_view, index, toRegister, register
+
+app_name = 'user'
 
 urlpatterns = [
+    path('toLogin/', login_view, name='toLogin'),
     path('login/', login_view, name='login'),
+    path('toRegister/', toRegister, name='toRegister'),
+    path('register/', register, name='register'),
+    path('index/', index, name='index'),
 ]
